015c3bbbfc8e3e989b2922577e3ffb7396f796a8
Florian Reiss <85802@supinfo.com>
2013-07-19 17:56:38 -0700

    Adding Constants.h
    Adding a file to contain constants.

---
+++ Linkedin2.0/Constants.h
@@ -0,0 +1,13 @@
+//
+//  Constants.h
+//  Linkedin2.0
+//
+//  Created by Florian Reiss on 19/07/13.
+//  Copyright (c) 2013 Florian Reiss. All rights reserved.
+//
+
+#ifndef Linkedin2_0_Constants_h
+#define Linkedin2_0_Constants_h
+
+
+#endif
